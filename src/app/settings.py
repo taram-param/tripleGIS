354a0dbd817860ea5bf96af4354193af5bc3d3b8
Taram Param <ekdzhumagulov@gmail.com>
2024-10-24 23:34:23 +0600

    change some settings for .env

--- src/app/settings.py
+++ src/app/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-=mtb#7hwukiwo^wa611b^e)6i#(^+@491xmz3--r(8$#!()_oy"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(int(os.environ.get("DEBUG", 0)))
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 
 
 # Application definition
